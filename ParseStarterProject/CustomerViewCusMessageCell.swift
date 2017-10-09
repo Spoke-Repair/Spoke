79570d5351652355ba93430c9ee39f9fbb2d1b1e
garrett <garretthuff41@yahoo.com>
2017-10-09 18:14:22 -0500

    added pictures into the resources folder

---
+++ ParseStarterProject/CustomerViewCusMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomerViewCusMessageCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Garrett Huff on 10/8/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewCusMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
