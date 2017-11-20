03318b998b1d90ed0e47e35674b386cac329e755
garrett <garretthuff41@yahoo.com>
2017-11-19 18:09:36 -0600

    MAJOR CHANGE TO STORY BOARD: removed BikeListController, replaces with a collection view called BikeListHome, still needs minor auto layout tweaks

---
+++ ParseStarterProject/BikeCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  BikeCollectionCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Garrett Huff on 11/19/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import UIKit
+
+class BikeCollectionCell: UICollectionViewCell {
+    @IBOutlet var textLabel: UILabel!
+    
+}
---
+++ ParseStarterProject/BikeListHome.swift
@@ -0,0 +1,178 @@
+//
+//  BikeListHome.swift
+//  
+//
+//  Created by Garrett Huff on 11/19/17.
+//
+
+import UIKit
+import Parse
+
+var bikeList = [String]()
+var customerBikeIDList = [String]()
+var customerIndex: Int = 0;
+var bikeObjectList = [BikeObject]()
+
+class BikeListHome: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    var indicator = UIActivityIndicatorView()
+
+   
+    @IBOutlet var bikeCollectionView: UICollectionView!
+    
+    @IBOutlet var visitBlogButton: UIButton!
+    @IBAction func visitBlogButton(_ sender: Any) {
+    //send the user to the blog
+    }
+    @IBAction func addBike(_ sender: Any) {
+        // let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //let vc = storyboard.instantiateViewController(withIdentifier: "addBikeVC")
+        //self.present(vc, animated: true, completion: nil)
+        self.performSegue(withIdentifier: "addBike", sender: self)
+    }
+    
+    
+    func activityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (bikeObjectList.count)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BikeCollectionCell", for: indexPath) as! BikeCollectionCell
+        cell.textLabel.text = bikeObjectList[indexPath.row].make
+        //set the color
+        switch((indexPath.row) % 4) {
+        
+        case 1:
+                print("CASE 1")
+                cell.contentView.backgroundColor = UIColor(displayP3Red: 237/255, green: 248/255, blue: 245/255, alpha: 1.0)
+
+        case 2:
+                print("CASE 2")
+                cell.contentView.backgroundColor = UIColor(displayP3Red: 255/255, green: 243/255, blue: 244/255, alpha: 1.0)
+
+        case 3:
+                print("CASE 3")
+                cell.contentView.backgroundColor = UIColor(displayP3Red: 253/255, green: 251/255, blue: 245/255, alpha: 1.0)
+
+        default:
+                print("CASE default")
+                cell.contentView.backgroundColor = UIColor(displayP3Red: 237/255, green: 248/255, blue: 245/255, alpha: 1.0)
+        }
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        customerIndex = indexPath.row
+        self.performSegue(withIdentifier: "customerViewBike", sender: self)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CommonUtils.addFCMTokenToParse()
+        activityIndicator()
+        visitBlogButton.layer.cornerRadius = 7
+        visitBlogButton.clipsToBounds = true
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        indicator.startAnimating()
+        indicator.backgroundColor = UIColor.white
+        //disable touch while the tableview is loading
+        self.view.isUserInteractionEnabled = false
+        
+        bikeObjectList.removeAll()
+        
+        let query = PFQuery(className: "Bike")
+        if let userID = PFUser.current()?.objectId {
+            print("user ID: "+userID)
+            query.whereKey("userID", equalTo: userID)
+            query.findObjectsInBackground(block: { (objects: [PFObject]?, error: Error?) in
+                if error == nil {
+                    if let objects = objects {
+                        for object in objects {
+                            //print("printing items from parse query")
+                            //print(object["userID"])
+                            //print(object["model"])
+                            let make = object["make"] as! String
+                            let model = object["model"] as! String
+                            let isOwned = object["isOwned"] as! Bool
+                            let size = object["size"] as! String
+                            let userId = object["userID"] as! String
+                            let bikeId = object.objectId!
+                            let bikeToAdd: BikeObject = BikeObject(make: make, model: model, size: size, isOwned: isOwned, userId: userId, bikeId: bikeId)
+                            
+                            
+                            if let picture = object["picture"] {
+                                let pImage = picture as! PFFile
+                                pImage.getDataInBackground(block: { (data: Data?, error: Error?) in
+                                    if let imageToSet = UIImage(data: data!) {
+                                        bikeToAdd.picture = imageToSet
+                                        print("Got image")
+                                       // self.tableView.reloadData()
+                                        self.bikeCollectionView.reloadData()
+                                    }
+                                    
+                                })
+                                
+                            }
+                            if(!(bikeList.contains(object["model"] as! String))){
+                                bikeList.append(object["model"] as! String)
+                                customerBikeIDList.append(object.objectId!)
+                            }
+                            bikeObjectList.append(bikeToAdd)
+                            print("ADDED BIKE TO LIST")
+                        }
+                        //reloadData()
+                        self.bikeCollectionView.reloadData()
+                        self.view.isUserInteractionEnabled = true
+                        self.indicator.stopAnimating()
+                        self.indicator.hidesWhenStopped = true
+                        
+                    }
+                }else {
+                    //there was an error
+                    print("There was an error...")
+                }
+            })
+            
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addBike" {
+            let navVC = segue.destination as? UINavigationController
+            _ = navVC?.viewControllers.first as! AddBikeViewController
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
