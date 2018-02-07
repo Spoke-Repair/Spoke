ef97167a55aa9edd8cb41ea1455f35eef180492b
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-02-07 01:29:24 -0600

    Shop phone vc

--- ParseStarterProject/LandingPageVC.swift
+++ ParseStarterProject/LandingPageVC.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class LandingPageVC: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,15 +24,4 @@ class LandingPageVC: UIViewController {
         triangle.fillColor = UIColor.cyan.cgColor
         self.view.layer.addSublayer(triangle)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ ParseStarterProject/NewShopAddressVC.swift
@@ -0,0 +1,32 @@
+//
+//  NewShopAddressVC.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Tim Gianitsos on 2/7/18.
+//  Copyright © 2018 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class NewShopAddressVC: UIViewController {
+
+    var user: PFUser!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
