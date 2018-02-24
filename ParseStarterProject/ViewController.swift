c3cc22681961906fb4e5f638f95891d33f57faf3
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-02-24 13:19:32 -0600

    Login with firebase auth but nothing happens to firestore

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -1,23 +1,24 @@
 /**
-* Copyright (c) 2015-present, Parse, LLC.
-* All rights reserved.
-*
-* This source code is licensed under the BSD-style license found in the
-* LICENSE file in the root directory of this source tree. An additional grant
-* of patent rights can be found in the PATENTS file in the same directory. hello
-*/
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory. hello
+ */
 
 import UIKit
 import Parse
+import FirebaseAuth
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     var signupMode = true
     var phoneNumberEntered = false
     var phoneNumber = ""
     //Used to store error message coming from account creation segue
     var errMsgStr: String?
-
+    
     @IBOutlet var otherLabel: UILabel!
     @IBOutlet var instructionLabel: UILabel!
     @IBOutlet var noAccountLabel: UILabel!
@@ -30,25 +31,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let newLength = (textField.text?.count)! + string.count - range.length
         var newString = ""
-
+        
         //alternative route if char was deleted from password field
         if(newLength < (textField.text?.count)! && phoneNumberEntered == true) {
             textField.text!.removeLast()
             return false
         }
-
+        
         //alternative logic to see if character was deleted
         if(newLength < (textField.text?.count)! && phoneNumberEntered == false) {
-
+            
             //(210) - 4
             if(newLength == 8){
                 print("Should remove spaces dash and parenthesis")
                 textField.text = textField.text!.replacingOccurrences(of: "-", with: "", options: NSString.CompareOptions.literal, range: nil)
                 textField.text = textField.text!.replacingOccurrences(of: " ", with: "", options: NSString.CompareOptions.literal, range: nil)
                 textField.text = textField.text!.replacingOccurrences(of: ")", with: "", options: NSString.CompareOptions.literal, range: nil)
-
+                
             }
-
+            
             //(210) - 432 -
             //length 14
             if(newLength == 14) {
@@ -96,12 +97,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 signUpButton.isHidden = true
                 noAccountLabel.isHidden = true
                 UIView.animate(withDuration: 1, animations: {
-
+                    
                     self.phoneNumber = textField.text!
                     self.phoneNumberEntered = true
                     textField.placeholder = "Enter a password"
                     textField.text = ""
-
+                    
                     //create button for end of text input
                     let button = UIButton(type: .custom)
                     button.setImage(UIImage(named: "arrow-right-gray.png"), for: .normal)
@@ -114,10 +115,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
                     textField.keyboardType = UIKeyboardType.default
                     textField.isSecureTextEntry = true
                     textField.rightViewMode = .always
-
+                    
                     self.username.center.x += self.view.bounds.width
                 }, completion: nil)
-
+                
                 return false
             }
         } else {
@@ -126,26 +127,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
             print("Password in function is: \(self.password)")
             return false
         }
-
+        
         return true
     }
     
-   
+    
     @IBAction func login(_ sender: Any) {
-            self.phoneNumber = self.phoneNumber.replacingOccurrences(of: "-", with: "", options: NSString.CompareOptions.literal, range: nil)
-            self.phoneNumber = self.phoneNumber.replacingOccurrences(of: " ", with: "", options: NSString.CompareOptions.literal, range: nil)
-            self.phoneNumber = self.phoneNumber.replacingOccurrences(of: "(", with: "", options: NSString.CompareOptions.literal, range: nil)
-            self.phoneNumber = self.phoneNumber.replacingOccurrences(of: ")", with: "", options: NSString.CompareOptions.literal, range: nil)
-
+        self.phoneNumber = self.phoneNumber.replacingOccurrences(of: "-", with: "", options: NSString.CompareOptions.literal, range: nil)
+        self.phoneNumber = self.phoneNumber.replacingOccurrences(of: " ", with: "", options: NSString.CompareOptions.literal, range: nil)
+        self.phoneNumber = self.phoneNumber.replacingOccurrences(of: "(", with: "", options: NSString.CompareOptions.literal, range: nil)
+        self.phoneNumber = self.phoneNumber.replacingOccurrences(of: ")", with: "", options: NSString.CompareOptions.literal, range: nil)
+        
         print("Phone number: \(self.phoneNumber)")
         print("Password: \(self.password)")
-
-            PFUser.logInWithUsername(inBackground: self.phoneNumber, password: self.password, block: {(user, error) in
-                if error != nil {
-                    CommonUtils.popUpAlert(message: error!.localizedDescription, sender: self)
-                } else {
-                    print("Login success")
-                    let type = user!["type"] as! String
+        
+        PFUser.logInWithUsername(inBackground: self.phoneNumber, password: self.password, block: {(user0, error0) in
+            guard error0 == nil else {
+                CommonUtils.popUpAlert(message: error0!.localizedDescription, sender: self)
+                return
+            }
+            print("Login success")
+            PFCloud.callFunction(inBackground: "generateFirebaseToken", withParameters: ["uid": PFUser.current()!.objectId!], block: {
+                (response: Any?, error1: Error?) -> Void in
+                guard error1 == nil else {
+                    CommonUtils.popUpAlert(message: error1!.localizedDescription, sender: self)
+                    return
+                }
+                print(response as! String)
+                Auth.auth().signIn(withCustomToken: response as! String) { (user1, error2) in
+                    guard error2 != nil else {
+                        CommonUtils.popUpAlert(message: error2!.localizedDescription, sender: self)
+                        return
+                    }
+                    let type = user0!["type"] as! String
                     if type == "employee"{
                         print("logging in as employee")
                         self.performSegue(withIdentifier: "loginEmployee", sender: self)
@@ -154,27 +168,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
                         print("logging in as customer")
                         self.performSegue(withIdentifier: "login", sender: self)
                     }
+
                 }
             })
+        })
     }
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.username.delegate = self
-
+        
         let path = UIBezierPath()
         path.move(to: CGPoint(x: 0, y: self.view.frame.height - 100))
         path.addLine(to: CGPoint(x: self.view.frame.width, y: self.view.frame.width))
         path.addLine(to: CGPoint(x: self.view.frame.width, y: self.view.frame.height))
         path.addLine(to: CGPoint(x: 0, y: self.view.frame.height))
         path.close()
-
+        
         let triangle = CAShapeLayer()
         triangle.path = path.cgPath
         triangle.fillColor = UIColor(red:0.79, green:0.93, blue:0.98, alpha:1.0).cgColor
         self.view.layer.addSublayer(triangle)
-
+        
         //Allows views to display in front of shape instead of hidden behind
         self.view.bringSubview(toFront: signUpButton)
         self.view.bringSubview(toFront: noAccountLabel)
@@ -197,9 +213,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
             CommonUtils.popUpAlert(message: errMsgStr, sender: self)
         }
     }
-
+    
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
 }
+
