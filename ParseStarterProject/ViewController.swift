d9e21e85804a077fecb390c82043229ffc9aac8b
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-03-01 15:35:42 -0600

    Enable back button animation from password

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -12,9 +12,9 @@ import Parse
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    var signupMode = true
     var currentlyEnteringPhone = true
     var phoneNumber = ""
+    
     //Used to store error messages to display when account creation fails
     var errMsgStr: String?
 
@@ -72,13 +72,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.text = ""
         UIView.animate(withDuration: 1, animations: {
             //create button for end of text input
-            let button = UIButton(type: .custom)
-            button.setImage(UIImage(named: "arrow-right-gray.png"), for: .normal)
-            button.imageEdgeInsets = UIEdgeInsetsMake(2, -16, 2, 10)
-            button.frame = CGRect(x: CGFloat(self.textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(14))
-            button.addTarget(self, action: #selector(self.login), for: .touchUpInside)
-            self.textField.rightView = button
+            let rightButton = UIButton(type: .custom)
+            rightButton.setImage(UIImage(named: "arrow-right-gray.png"), for: .normal)
+            rightButton.imageEdgeInsets = UIEdgeInsetsMake(0, -16, 0, 10)
+            rightButton.frame = CGRect(x: CGFloat(self.textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(14))
+            rightButton.addTarget(self, action: #selector(self.login), for: .touchUpInside)
+            self.textField.rightView = rightButton
             self.textField.rightViewMode = .always
+            
+            //create button to go back
+            let leftButton = UIButton(type: .custom)
+            leftButton.setImage(UIImage(named: "arrow-right-gray.png")?.withHorizontallyFlippedOrientation(), for: .normal)
+            leftButton.imageEdgeInsets = UIEdgeInsetsMake(0, 4, 0, -10)
+            leftButton.frame = CGRect(x: CGFloat(self.textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(14))
+            leftButton.addTarget(self, action: #selector(self.goBack), for: .touchUpInside)
+            self.textField.leftView = leftButton
+            self.textField.leftViewMode = .always
 
             self.instructionLabel.text = "ENTER YOUR PASSWORD"
             self.otherLabel.text = "Welcome"
@@ -87,7 +96,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
             self.textField.center.x += self.view.bounds.width
         }, completion: nil)
     }
-   
+    
+    @objc private func goBack() {
+        UIView.animate(withDuration: 1, animations: {
+            
+            //Reinstate old prompts
+            self.currentlyEnteringPhone = true
+            self.phoneNumber = ""
+            self.instructionLabel.text = "LOGIN TO ACCOUNT"
+            self.otherLabel.text = "Enter the phone number that identifies your account"
+            self.signUpButton.isHidden = false
+            self.noAccountLabel.isHidden = false
+            
+            //Remove left button
+            self.textField.text = ""
+            self.textField.placeholder = "Phone number"
+            self.textField.leftView = nil
+            self.textField.leftViewMode = .never
+            self.textField.keyboardType = .numberPad
+            self.textField.isSecureTextEntry = false
+            self.textField.center.x -= self.view.bounds.width
+            
+            //Create button for right end of text input
+            let button = UIButton(type: .custom)
+            button.setImage(UIImage(named: "arrow-right-gray.png"), for: .normal)
+            button.imageEdgeInsets = UIEdgeInsetsMake(0, -16, 0, 10)
+            button.frame = CGRect(x: CGFloat(self.textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(14))
+            button.addTarget(self, action: #selector(self.proceedToPassword), for: .touchUpInside)
+            self.textField.rightView = button
+            self.textField.rightViewMode = .always
+            
+        }, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.textField.delegate = self
@@ -99,7 +140,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
             //create button for end of text input
             let button = UIButton(type: .custom)
             button.setImage(UIImage(named: "arrow-right-gray.png"), for: .normal)
-            button.imageEdgeInsets = UIEdgeInsetsMake(2, -16, 2, 10)
+            button.imageEdgeInsets = UIEdgeInsetsMake(0, -16, 0, 10)
             button.frame = CGRect(x: CGFloat(self.textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(14))
             button.addTarget(self, action: #selector(self.proceedToPassword), for: .touchUpInside)
             self.textField.rightView = button
